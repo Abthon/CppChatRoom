21066df41d63dc675f988370229441efac7b8a54
Abthon <walelignabenezer@gmail.com>
2022-06-03 06:15:02 -0400

    First Commit

---
+++ Client.cpp
@@ -0,0 +1,97 @@
+#include<unistd.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include <arpa/inet.h>
+#include<iostream>
+#include<netdb.h>
+#include<stdlib.h>
+#include<string.h>
+#include<thread>
+using namespace std;
+
+
+// Our Function defenition
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+
+void sendMsg(int server)
+{
+    while (1){
+        string msg;
+        getline(cin,msg);
+        write(server,msg.c_str(),strlen(msg.c_str()));
+    }
+}
+
+
+void reciveMsg(int server, string client)
+{
+    while(1){
+        char msg[1024] {0};
+        read(server,msg,sizeof(msg)-1);
+        cout << msg << endl;
+    }
+    
+}
+
+
+// Our Main FUnction
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n,name;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    
+
+    char buffer[1024]{0};
+
+    if (argc < 3)
+    {
+        fprintf(stderr,"usage %s hostname port\n", argv[0]);
+        exit(0);
+    }
+
+    portno = atoi(argv[2]);
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0)
+    {
+        error("ERROR opening socket");
+    }
+
+    server = gethostbyname(argv[1]);
+
+    if (server == NULL){
+        fprintf(stderr, "Errror, no such host\n");
+        exit(0);
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portno);
+
+    if (connect(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr)) < 0)
+    {
+        error("ERROR connecting");
+    }
+
+    string userName;
+    cout << "Please Enter your userName: ";
+    getline(cin,userName);
+    write(sockfd,userName.c_str(),strlen(userName.c_str()));
+
+
+    thread recvMsg(reciveMsg,sockfd,userName);
+    recvMsg.detach();
+    sendMsg(sockfd);
+
+    close(sockfd);
+    cout << "Connection is Closed!" << endl;
+
+    return 0;
+}
+
---
+++ Server.cpp
@@ -0,0 +1,116 @@
+#include<thread> //  Header file for Threading 
+#include<cstring> // Header file for accessing c_string functions
+#include<iostream> // Header file for input/out operation
+#include<unistd.h> // Header file to write a message to the client socket ( read, write )
+#include<sys/socket.h> // Header file for creating a socket object
+#include<netinet/in.h> // Header file to acess INADDR_IN
+#include<chrono> 
+#include<vector>
+using namespace std;
+
+int activeConnections = 0;
+void error(const char* msg); 
+void activeConnectionCounter();
+void handle_connection(int conn, char name[1024]);
+
+int main(int argc, char *argv[]){
+
+
+    int server,client,portno,backlog;
+    struct sockaddr_in serv_addr, cli_addr;
+    char userName[1024] {0};
+
+
+    // Creating Socket
+    server = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (server < 0)
+    {
+        error("Error occured while creating the socket object! ");
+    }
+
+    portno = atoi(argv[1]);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+
+    // Binding socket
+    if (bind(server, (struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+    {
+        error("ERROR occured while binding the socket! ");
+        exit(1);
+    }
+
+    cout << endl;
+    cout << "[ STARTING ] Server is starting......" << endl << endl;
+
+    // Listining for incoming connections
+    listen(server,backlog);
+    cout << "Listning for incoming connections..." << endl;
+
+    while(1){
+
+        //Accepting socket
+        client = accept(server,(struct sockaddr *)NULL,NULL);
+        if(client < 0){
+            error("Error occured while accepting a new socket! ");
+        }
+
+
+        read(client,userName,sizeof(userName)-1);
+        cout << "New connection is Established! " << endl;
+        cout << userName << " Joined the chat Room! " << endl;
+        thread workerThread(handle_connection,client,userName);
+        workerThread.detach();
+
+    }
+
+    //Closing conneciton
+    close(client);
+    cout << "Connection Closed!";
+
+
+
+    return 0;
+}
+
+
+void handle_connection(int conn,char name[1024]){
+    cout << "User Id " << conn << endl;
+    activeConnectionCounter();
+    char userName[1024];
+    string messageToSent;
+    strcpy(userName,name);
+    bool connected = true; 
+    cout << "Active Users increased from " << activeConnections -1 << " to " << activeConnections <<endl;
+    while (connected){
+        cout << "GEba"<< endl;
+        char buffer[1024] {0};
+        read(conn,buffer,sizeof(buffer)-1);
+
+        if(strcmp(buffer,"Disconnect") == 0){
+            connected = false;
+        }
+
+        messageToSent.append(userName);
+        messageToSent.append(": ");
+        messageToSent.append(buffer);
+        // write(conn,messageToSent.c_str(),messageToSent.length());
+        // write(conn,)
+        cout << userName << ": " << buffer << endl;
+    }
+
+    close(conn);
+}  
+
+void activeConnectionCounter(){
+    activeConnections +=1; 
+}
+
+void error(const char* msg)
+{
+    cout << msg <<endl;
+    exit(1);
+}
